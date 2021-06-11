9302352bce7b2bb39c6576c6ea97303dae03d2e2
Nong Xiang Shen <michael7105@gmail.com>
2021-06-11 15:52:44 +0800

    update

--- run_multi_udp_.sh
+++ run_multi_udp_.sh
@@ -3,37 +3,39 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
 trap "kill 0" INT
 
-prefix_tcl="test10"
+prefixTCL="test10"
 
 cd sumo-learning
 git pull
 cp ./multi_udp.cc ../scratch/
-cp ./${prefix_tcl}/${prefix_tcl}.tcl ../scratch/
+cp ./${prefixTCL}/${prefixTCL}.tcl ../scratch/
 cd ..
 
 prefixName="multi_udp_sdn"
 isSdnEnabled=true
 if [ $isSdnEnabled == false ]
 then
-        prefixName="multi_udp_lte"
+	prefixName="multi_udp_lte"
 fi
 
 outLogDir="outLog_${prefixName}"
 echo "isSdnEnabled = ${isSdnEnabled}"
 echo "outLogDir = ${outLogDir}"
-mkdir $outLogDir
+mkdir -p $outLogDir
 cp ./sumo-learning/merge_multi.sh $outLogDir
 cp ././sumo-learning/log_to_csv.py $outLogDir
 
 logName="log.${prefixName}"
 outLogPath="${outLogDir}/${logName}"
+animFile="anim.${prefixName}_${i}"
 echo "logName = ${logName}"
 echo "outLogDir = ${outLogDir}"
+echo "animFile = ${animFile}"
 startUe=0
 endUe=100
 echo "startUe = ${startUe}"
 echo "endUe = ${endUe}"
-(./waf --run "scratch/lteudp_multi_udp --simTime=60 --numberOfUes=100 --startUe=${startUe} --endUe=${endUe} --isSdnEnabled=${isSdnEnabled} --isDownlink=true --isUplink=true --interAppInterval=0.001 --interPacketInterval=1 --nPayloadBytes=1036" > $outLogPath 2>&1) &
+(./waf --run "scratch/lteudp_multi_udp --simTime=60 --numberOfUes=100 --startUe=${startUe} --endUe=${endUe} --isSdnEnabled=${isSdnEnabled} --isDownlink=true --isUplink=true --animFile=${animFile} --interAppInterval=0.001 --interPacketInterval=1 --nPayloadBytes=1036" > $outLogPath 2>&1) &
 
 echo "isSdnEnabled = ${isSdnEnabled}"
 echo "outLogDir = ${outLogDir}"
--- run_multi_udp_0.sh
+++ run_multi_udp_0.sh
@@ -3,15 +3,23 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
 trap "kill 0" INT
 
+prefixTCL="test10"
+
 cd sumo-learning
 git pull
 cp ./ns3lte/lteudp_multi_udp.cc ../scratch/
-cp ./test10/test10.tcl ../scratch/
+cp ./${prefixTCL}/${prefixTCL}.tcl ../scratch/
 cd ..
 
-isSdnEnabled=true
 prefixName="lteudp_multi_udp_sdn"
+isSdnEnabled=true
+if [ $isSdnEnabled == false ]
+then
+	prefixName="lteudp_multi_udp_lte"
+fi
+
 outLogDir="outLog_${prefixName}"
+echo "isSdnEnabled = ${isSdnEnabled}"
 echo "outLogDir = ${outLogDir}"
 mkdir $outLogDir
 cp ./sumo-learning/merge_multi.sh $outLogDir
@@ -29,8 +37,15 @@ do
     endUe=$(($i*5+5))
     echo "startUe = ${startUe}"
     echo "endUe = ${endUe}"
-    (./waf --run "scratch/lteudp_multi_udp --numberOfUes=100 --startUe=${startUe} --endUe=${endUe} --isSdnEnabled=${isSdnEnabled} --isDownlink=true --isUplink=true --animFile=${animFile} --interAppInterval=0.01 --interPacketInterval=50" > $outLogPath 2>&1) &
+    (./waf --run "scratch/lteudp_multi_udp --simTime=60 --numberOfUes=100 --startUe=${startUe} --endUe=${endUe} --isSdnEnabled=${isSdnEnabled} --isDownlink=true --isUplink=true --animFile=${animFile} --interAppInterval=0.001 --interPacketInterval=1 --nPayloadBytes=1036" > $outLogPath 2>&1) &
+    if [ $i == 0 ]
+    then
+	    sleep 20s
+    fi
     sleep 1s
 done
+
+echo "isSdnEnabled = ${isSdnEnabled}"
+echo "outLogDir = ${outLogDir}"
 
 wait
